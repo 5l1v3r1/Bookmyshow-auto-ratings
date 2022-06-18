4e72282157eb88af530286758255fecf31436a4f
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-06-18 19:58:00 +0530

    Update beast.py

--- beast.py
+++ beast.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/beast/ET00311733")
+driver.get("https://in.bookmyshow.com/bengaluru/movies/beast/ET00311733") #replace with the url of which movie you want to auto rate
 
 input=driver.find_element_by_xpath('//*[@id="wzrk-confirm"]').click()
 
@@ -17,7 +17,7 @@ input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/di
 
 time.sleep(5)
 
-input=driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("vk123@gmail.com") #replace with your email
+input=driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("vk123@gmail.com") #replace with your email otp doesn't auto fill you must fill it
 
 input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
 
@@ -26,6 +26,8 @@ time.sleep(15)
 
 input=driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section[2]/div[1]/button/span').click()
 
-time.sleep(8)
+#drag the heart symbol to required percentage(if i find auto drag i will update it)
+
+time.sleep(7)
 
 input=driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/div/div[2]/div/div/div/div[3]/div/button').click()
