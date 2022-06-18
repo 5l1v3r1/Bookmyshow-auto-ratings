652e1f4c5667ee7735a15b8e4a3b58990e643c54
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-06-18 19:49:00 +0530

    Update beast.py

--- beast.py
+++ beast.py
@@ -1,23 +1,31 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/beast/ET00311733") #Which Movie You Want To Rate Now Paste That Url
+driver.get("https://in.bookmyshow.com/login/?referer=/bengaluru/movies/beast/ET00311733")
 
-input=driver.find_element_by_xpath('//*[@id="wzrk-confirm"]')
-input.click()
+input=driver.find_element_by_xpath('//*[@id="wzrk-confirm"]').click()
 
-input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div')
-input.click()
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section[2]/div[1]/button/span').click()
 
-input=driver.find_element_by_xpath('//*[@id="emailId"]')
-input.send_keys('putyrid123@gmail.com') #Replace With Your Mail OTP Will Not Be Auto Fill You Must Enter It👍
+time.sleep(5)
 
-input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button')
-input.click()
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/div/div[1]/div/div[2]/div/div').click()
 
-#It Ask Rate Now Click On That & Rate The Movie And Click Submit (If I Find Drag Automatically I Will Update It👍)
+time.sleep(5)
 
+input=driver.find_element_by_xpath('//*[@id="emailId"]').send_keys("vk123@gmail.com") #replace with your email
 
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
 
+driver.maximize_window()
+time.sleep(15)
 
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section[2]/div[1]/button/span').click()
+
+time.sleep(8)
+
+input=driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/div/div[2]/div/div/div/div[3]/div/button').click()
