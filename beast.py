f600f6eadb47fd3fbc5b78b29d230685100a99b3
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-08-07 22:06:12 +0530

    Update beast.py

--- beast.py
+++ beast.py
@@ -21,32 +21,28 @@ driver.find_element_by_css_selector('#emailId').send_keys("yashuvirat@gmail.com"
 
 time.sleep(3)
 
-#otp must enter you only its can't be automated
-
 driver.find_element_by_xpath('//*[@id="super-container"]/div[3]/div/div/div/div[2]/form/div[2]/button').click()
 
-time.sleep(10)
+time.sleep(20)
 
 driver.maximize_window()
 time.sleep(10)
 
-#driver.find_element_by_css_selector('#super-container > div.sc-11q8erw-0.jJDMQQ > section.sc-qswwm9-0.ljnioe > div > div > div.sc-qswwm9-5.ehwnas > section.sc-ycjzp1-0.duexLW > div.sc-ycjzp1-1.fdWjQB > button > span').click()
-
 driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/section[1]/div/div/div[2]/section[2]/div[1]/button/span').click()
 
-time.sleep(7)
+time.sleep(8)
 
-#currently working on auto drag and rating so plz rate in that section i will coming soon
+slider = driver.find_element_by_css_selector("#range")
 
-#driver.find_element_by_css_selector('#super-container > div.sc-11q8erw-0.jJDMQQ > div > div.sc-10qvp23-0.bAXHgy > div > div > div > div.main.scroll-out.false.desktop-main > div > div.rating-wrapper.false > div.rating-section > div > div.rating-slider.desktop-rating-slider > div.range-thumb').click()
+move = ActionChains(driver)
+move.click_and_hold(slider).move_by_offset(10,90).release().perform() #change to your required ratings
 
-#time.sleep(8)
+time.sleep(5)
 
 driver.find_element_by_xpath('//*[@id="super-container"]/div[2]/div/div[2]/div/div/div/div[3]/div/button').click()
 
 time.sleep(50)
 
 driver.close()
-
 
 
